7dc4e6666f67ebef09fa851b124be3f1c366f5e8
Stélian CAMARA DIT PINTO <s.camaraditpinto@estia.local>
2017-06-13 11:54:07 +0200

    adding the ellipse path function

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ Intersection =
 		return ( end1-0.00000001 <= a && a <= end2 + 0.000000001) || ( end2-0.000000001 <= a && a <= end1+0.00000001 )
 	}
 
-	function inflexionPoint( bezier ){
+	function inflexionPointBezier( bezier ){
 		var Ay = bezier.cubicY.a;
 		var By = bezier.cubicY.b;
 		var Cy = bezier.cubicY.c;
@@ -28,7 +28,7 @@ Intersection =
 		var solution = [];
 		var tX = [];
 		var tY = [];
-
+		//test if derivate = 0 on Y
 		if( Ay != 0 && By != 0 ){
 			var dAy = 3*Ay;
 			var dBy = 2*By;
@@ -37,7 +37,7 @@ Intersection =
 			var dBy = 2*By;
 			tY = linearResolution( dBy, Cy );
 		}
-
+		//test if derivate = 0 on X
 		if( Ax != 0 && Bx != 0 ){
 			var dAx = 3*Ax;
 			var dBx = 2*Bx;
@@ -46,7 +46,6 @@ Intersection =
 			var dBx = 2*Bx;
 			tX = linearResolution( dBx, Cx );
 		}
-
 		for( i in tX ){
 			if( tX[i] != -1 && tX[i] != 0 && tX[i] != 1 ){
 				solution.push( tX[i] );
@@ -58,6 +57,10 @@ Intersection =
 			}
 		}
 		return solution;
+	}
+
+	function inflexionPointEllipse( ellipse ){
+		return [ 0, Math.PI/2, Math.PI, (3*Math.PI)/2, 2*Math.PI ];
 	}
 
 	//return solution list for a linear equation
@@ -74,12 +77,9 @@ Intersection =
 	}
 	//return solution list for a quadratic equation
 	function quadraticResolution( A, B, C ){
-		//console.log( " A : " + A + "  B : " + B + "  C : " + C );
 		//discriminant
 		var D = (B*B) - 4*A*C;
-		//console.log( " discriminant : " + D );
 		var results = [];
-
 		if( D > 0 ){
 			results[0] = ( -B-Math.sqrt(D) ) / ( 2*A );
 			results[1] = ( -B+Math.sqrt(D) ) / ( 2*A );
@@ -87,7 +87,6 @@ Intersection =
 			results[0] = (-B)/(2*A);
 		}
 		for ( i in results ){
-			//console.log( " quadratic : " + results[i] );
         	if( results[i]<-0.00000000001 || results[i]>1.00000000001 ){
         		results[i]=-1;
         	}
@@ -107,25 +106,16 @@ Intersection =
 		var D1 = ( 3*coefficient2 - Math.pow( coefficient1, 2 ) )/9;
 		var D2 = ( 9*coefficient1*coefficient2 - 27*coefficient3 - 2*Math.pow( coefficient1, 3 ) )/54;
 		var D = Math.pow( D1, 3 ) + Math.pow( D2, 2 );
-		/*console.log("D1 " + D1 + " D2 " + D2 + " D " + D );*/
-
 		var results = [];
-
 		if( D >= 0 ){
 			var solutionPlus = sgn( D2 + Math.sqrt(D) )*Math.pow( Math.abs( D2 + Math.sqrt(D) ), (1/3) );
 			var solutionMoins = sgn( D2 - Math.sqrt(D) )*Math.pow( Math.abs( D2 - Math.sqrt(D) ), (1/3) );
-			/*console.log( "solutionPlus " + solutionPlus );*/
-			/*console.log( "solutionMoins " + solutionMoins );*/
 			results[0] = -coefficient1/3 + ( solutionPlus + solutionMoins );//real root
 			results[1] = -coefficient1/3 - ( solutionPlus + solutionMoins )/2;//real part of complex root
-
 			var imaginary = Math.abs(Math.sqrt(3)*(solutionPlus - solutionMoins)/2);// complex part of complex root
-			/*console.log( "imaginary " + imaginary );*/
 			if( imaginary != 0 ){
 				results[1] = -1;
 			}
-
-
 		} else {
 			var theta = Math.acos( D2/Math.sqrt( -Math.pow( D1, 3 ) ) );
 			//real distinct roots
@@ -135,12 +125,10 @@ Intersection =
 		}
 		//test if solution belong [0,1]
 		for ( var i=0; i<3; i++ ){
-			/*console.log( results[i] );*/
         	if( results[i]<-0.00000000001 || results[i]>1.00000000001 ){
         		results[i]=-1;
         	}
         }
-
         return results;
 	}
 
@@ -154,6 +142,23 @@ Intersection =
 	ParametricCubic.prototype.apply = function( t ){
 		return this.d + t * ( this.c + t * ( this.b + t * this.a ) );
 	}
+
+	function ParametricX( u, a ){
+		this.u = u;
+		this.a = a; 
+	}
+	ParametricX.prototype.apply = function( t ){
+		return this.u + this.a*Math.cos(t);
+	}
+	function ParametricY( v, b ){
+		this.v = v;
+		this.b = b; 
+	}
+	ParametricY.prototype.apply = function( t ){
+		return this.v + this.b*Math.sin(t);
+	}
+
+	//create a bezier curve with is parametric coefficients
 	function Bezier( x0, y0, x1, y1, x2, y2, x3, y3 ){
 		var a = -y0 + 3*y1 - 3*y2 + y3;//coefficient t^3
 		var b = 3*y0 - 6*y1 + 3*y2;//coefficient t^2
@@ -166,26 +171,33 @@ Intersection =
 		d = x0;
 		this.cubicX = new ParametricCubic( a,b,c,d );
 	}
+
+	//create a bezier curve with is parametric coefficients
+	function Ellipse( a, b, u, v ){
+		this.parametricX = new ParametricSin( u, a );
+		this.parametricY = new ParametricCos( v, b );
+	}
+
 	function BezierPoint( t, bezier ){
 		this.t = t; 
 		this.x = bezier.cubicX.apply( t );
 		this.y = bezier.cubicY.apply( t );
 	}
+
+	function EllipsePoint( t, ellipse ){
+		this.t = t; 
+		this.x = ellipse.parametricX.apply( t );
+		this.y = ellipse.parametricY.apply( t );
+	}
+
 	function distanceT( t1, t2 ){
 		var x = t1.x - t2.x; 
 		var y = t2.y - t1.y; 
 		return  x*x + y*y ;
 	}
 
-function find2( ) {
-	console.log( arguments )
-	var results = findIntersectionByDichotomy.apply( this, arguments ) 
-	console.log( results )
-	return results ;
-}
-
-	function findIntersectionByDichotomy( bezierT, bezierS, t1, t2, s1, s2 ){
-		
+	//find the intersection point between two curve using dichotomy methode
+	function findIntersectionByDichotomyBezier( bezierT, bezierS, t1, t2, s1, s2 ){
 		if( ( !insideInterval( t1.x, s1.x, s2.x ) 
 		   && !insideInterval( t2.x, s1.x, s2.x ) 
 		   && !insideInterval( s1.x, t1.x, t2.x ) 
@@ -195,7 +207,6 @@ function find2( ) {
 		   && !insideInterval( s1.y, t1.y, t2.y ) 
 		   && !insideInterval( s2.y, t1.y, t2.y ) ) ) {
 			//no intersection 
-
 			return []; 
 		} else {
 			//Atomic square
@@ -205,13 +216,61 @@ function find2( ) {
 			//Subdivisable squares
 				var middleT = new BezierPoint( (t1.t  + t2.t)/2, bezierT );
 				var middleS = new BezierPoint( (s1.t  + s2.t)/2, bezierS );
-				var r1 = findIntersectionByDichotomy( bezierT, bezierS, t1, middleT, s1, middleS )
-				var r2 = findIntersectionByDichotomy( bezierT, bezierS, middleT, t2, s1, middleS )
-				var r3 = findIntersectionByDichotomy( bezierT, bezierS, t1, middleT, middleS, s2 )
-				var r4 = findIntersectionByDichotomy( bezierT, bezierS, middleT, t2, middleS, s2 )
+				var r1 = findIntersectionByDichotomyBezier( bezierT, bezierS, t1, middleT, s1, middleS )
+				var r2 = findIntersectionByDichotomyBezier( bezierT, bezierS, middleT, t2, s1, middleS )
+				var r3 = findIntersectionByDichotomyBezier( bezierT, bezierS, t1, middleT, middleS, s2 )
+				var r4 = findIntersectionByDichotomyBezier( bezierT, bezierS, middleT, t2, middleS, s2 )
 				return Array.prototype.concat( r1, r2, r3, r4 );	
 			}
 		}
+	}
+
+	function findIntersectionByDichotomyEllipse( bezier, ellipse, b1, b2, e1, e2 ){
+		if( ( !insideInterval( b1.x, e1.x, e2.x ) 
+		   && !insideInterval( b2.x, e1.x, e2.x ) 
+		   && !insideInterval( e1.x, b1.x, b2.x ) 
+		   && !insideInterval( e2.x, b1.x, b2.x ) )
+		 || ( !insideInterval( b1.y, e1.y, e2.y ) 
+		   && !insideInterval( b2.y, e1.y, e2.y ) 
+		   && !insideInterval( e1.y, b1.y, b2.y ) 
+		   && !insideInterval( e2.y, b1.y, b2.y ) ) ) {
+			//no intersection 
+			return []; 
+		} else {
+			//Atomic square
+			if( distanceT( b1, b2 ) < 0.0000000000001 ){
+				return [[ b1, b2, e1, e2 ]]
+			} else {
+			//Subdivisable squares
+				var middleB = new BezierPoint( (b1.t  + b2.t)/2, bezier );
+				var middleE = new EllipsePoint( (e1.t  + e2.t)/2, ellipse );
+				var r1 = findIntersectionByDichotomyEllipse( bezier, ellipse, b1, middleB, e1, middleE )
+				var r2 = findIntersectionByDichotomyEllipse( bezier, ellipse, middleB, b2, e1, middleE )
+				var r3 = findIntersectionByDichotomyEllipse( bezier, ellipse, b1, middleB, middleE, e2 )
+				var r4 = findIntersectionByDichotomyEllipse( bezier, ellipse, middleB, b2, middleE, e2 )
+				return Array.prototype.concat( r1, r2, r3, r4 );	
+			}
+		}
+	}
+
+	//add the results in solutions and remove the one too much closer from one an other
+	function ajouterSolutions( solutions, result ){
+		for( var intersection of result ){
+			var add = true;
+
+			for( var solution of solutions ){
+				if( ( Math.floor( 10000 * intersection[0].x ) == Math.floor( 10000 * solution[0] ) )
+				 && ( Math.floor( 10000 * intersection[0].y ) == Math.floor( 10000 * solution[1] ) ) ){
+					add = false;
+				}
+			}
+
+			if( add ){
+				solutions.push( [ intersection[0].x, intersection[0].y ] );
+			}
+		}
+
+		return solutions
 	}
 
 	//return IntersectionData
@@ -357,23 +416,71 @@ function find2( ) {
 
 		return intersectionData;
 	}
-
-	function ajouterSolutions( solutions, resultat ) {
-		/**A copier a la fin **/ 
-		for( var intersection of resultat ){
-			//console.log( Math.floor( 100000 * intersection[0].x ),  Math.floor( 100000 * intersection[0].y ) );
-			var add = true;
-			for( var solution of solutions ){
-				if( ( Math.floor( 10000 * intersection[0].x ) == Math.floor( 10000 * solution[0] ) )
-				 && ( Math.floor( 10000 * intersection[0].y ) == Math.floor( 10000 * solution[1] ) ) ) {
-					add = false;
-				}
-			}
-			if( add ){
-				solutions.push( [ intersection[0].x, intersection[0].y ] );
+	//return coords of intersection between bezier curve and ellispse
+	intersectionLibrary.intersectionBezierEllipse = function( path, ellispse ){
+		var points1 = path.array();
+		var Xp0;
+		var Yp0;
+		var Xp1;
+		var Yp1;
+		var Xp2;
+		var Yp2;
+		var Xp3;
+		var Yp3;
+		//recuperation of the bezier curve control point
+		for( i in points1.value ){
+			if( points1.value[i][0] == "C" ){
+				Xp0 = points1.value[i-1][points1.value[i-1].length-2];
+				Yp0 = points1.value[i-1][points1.value[i-1].length-1];
+				Xp1 = points1.value[i][1];
+				Yp1 = points1.value[i][2];
+				Xp2 = points1.value[i][3];
+				Yp2 = points1.value[i][4];
+				Xp3 = points1.value[i][5];
+				Yp3 = points1.value[i][6];
 			}
 		}
-		return solutions
+
+		
+		bezier = new Bezier( Xp0 , Yp0 , Xp1 , Yp1 , Xp2 , Yp2 , Xp3 , Yp3 );
+		ellipse = new Ellipse( a, b, u, v );
+
+		p1 = new BezierPoint( 0, bezier );
+		p2 = new BezierPoint( 1, bezier );
+		p3 = new EllipsePoint( 0, ellipse );
+		p4 = new EllipsePoint( 2*Math.PI, ellipse );
+		//looking for inflexion points on both curves
+		inflexionPoints1 = inflexionPointBezier( bezier );
+		inflexionPoints2 = inflexionPointEllipse( ellipse );
+
+		if( inflexionPoints1.length == 0 ){
+			for( var i = 1; i < inflexionPoints2.length; i++ ){
+				var p3 = new EllipsePoint( inflexionPoints2[i-1], ellipse );
+				var p4 = new EllipsePoint( inflexionPoints2[i], ellipse );
+				var result = findIntersectionByDichotomyEllipse( bezier, ellipse, p1, p2, p3, p4 );
+				solutions = ajouterSolutions( solutions, result )
+			}
+		//same if inflexion point on both curves
+		} else {
+			inflexionPoints1.push( 0, 1 );
+			inflexionPoints1.sort(function(a,b) { return a>b });
+			for( var i = 1; i < inflexionPoints1.length; i++ ){
+				for( var j = 1; j < inflexionPoints2.length; j++ ){
+					var p1 = new BezierPoint( inflexionPoints1[i-1], bezier );
+					var p2 = new BezierPoint( inflexionPoints1[i], bezier );
+					var p3 = new EllipsePoint( inflexionPoints2[j-1], ellipse );
+					var p4 = new EllipsePoint( inflexionPoints2[j], ellipse );
+					var result = findIntersectionByDichotomyBezier( bezier, ellipse, p1, p2, p3, p4 );
+					solutions = ajouterSolutions( solutions, result )
+				}
+			}
+		}
+		if( solutions.length != 0 ){
+				var intersectionData = new IntersectionData( "point", solutions );
+			} else {
+				var intersectionData = new IntersectionData( "empty", [ [] ] );
+			}
+		return intersectionData;
 	}
 
 	//retrun coords of intersection between bezier curve and another bezier curve
@@ -427,71 +534,59 @@ function find2( ) {
 
 		bezier1 = new Bezier( Xp0 , Yp0 , Xp1 , Yp1 , Xp2 , Yp2 , Xp3 , Yp3 );
 		bezier2 = new Bezier( Xp4 , Yp4 , Xp5 , Yp5 , Xp6 , Yp6 , Xp7 , Yp7 );
-		//console.log( bezier1, bezier2 );
 
 		p1 = new BezierPoint( 0, bezier1 );
 		p2 = new BezierPoint( 1, bezier1 );
 		p3 = new BezierPoint( 0, bezier2 );
 		p4 = new BezierPoint( 1, bezier2 );
-		//console.log( p1, p2, p3, p4 );
+		//looking for inflexion points on both curves
+		inflexionPoints1 = inflexionPointBezier( bezier1 );
+		inflexionPoints2 = inflexionPointBezier( bezier2 );
 
-		inflexionPoints1 = inflexionPoint( bezier1 );
-		inflexionPoints2 = inflexionPoint( bezier2 );
-		//console.log( inflexionPoints1.length, inflexionPoints2.length );
-		//console.log( inflexionPoints1, inflexionPoints2 )
 		var solutions = [] 
+		//test if there is no inflexion point
 		if( inflexionPoints1.length == 0 && inflexionPoints2.length == 0 ){
-			var resultat = find2( bezier1, bezier2, p1, p2, p3, p4 );
-			console.log( resultat );
-			solutions = ajouterSolutions( solutions, resultat )
-
-
+			var result = findIntersectionByDichotomyBezier( bezier1, bezier2, p1, p2, p3, p4 );
+			solutions = ajouterSolutions( solutions, result )
+		//if inflexion point on first curve
 		} else if( inflexionPoints2.length == 0 ){
 			inflexionPoints1.push( 0, 1 );
 			inflexionPoints1.sort(function(a,b) { return a>b });
-			console.log( "inflexionPoints1 = " + inflexionPoints1 );
 			for( var i = 1; i < inflexionPoints1.length; i++ ){
+				//calculate new bezier point at the coordonate of the inflexion point
 				var p1 = new BezierPoint( inflexionPoints1[i-1], bezier1 );
-				console.log(p1)
 				var p2 = new BezierPoint( inflexionPoints1[i], bezier1 );
-				console.log(p2)
-				var resultat = find2( bezier1, bezier2, p1, p2, p3, p4 );
-				solutions = ajouterSolutions( solutions, resultat )
-				console.log( resultat )
+				//call the intersection function on all new intervals
+				var result = findIntersectionByDichotomyBezier( bezier1, bezier2, p1, p2, p3, p4 );
+				solutions = ajouterSolutions( solutions, result )
 			}
+		//same if inflexion point on the second
 		} else if( inflexionPoints1.length == 0 ){
 			inflexionPoints2.push( 0, 1 );
 			inflexionPoints2.sort(function(a,b) { return a>b });
-			console.log( " inflexionPoints2 = " + inflexionPoints2 );
 			for( var i = 1; i < inflexionPoints2.length; i++ ){
 				var p3 = new BezierPoint( inflexionPoints2[i-1], bezier2 );
-				console.log(p3)
 				var p4 = new BezierPoint( inflexionPoints2[i], bezier2 );
-				console.log(p4)
-				var resultat = find2( bezier1, bezier2, p1, p2, p3, p4 );
-				solutions = ajouterSolutions( solutions, resultat )
-				console.log( resultat )
+				var result = findIntersectionByDichotomyBezier( bezier1, bezier2, p1, p2, p3, p4 );
+				solutions = ajouterSolutions( solutions, result )
 			}
+		//same if inflexion point on both curves
 		} else {
 			inflexionPoints1.push( 0, 1 );
 			inflexionPoints2.push( 0, 1 );
 			inflexionPoints1.sort(function(a,b) { return a>b });
 			inflexionPoints2.sort(function(a,b) { return a>b });
-			console.log( " inflexionPoints1 = " + inflexionPoints1 );
-			console.log( " inflexionPoints2 = " + inflexionPoints2 );
 			for( var i = 1; i < inflexionPoints1.length; i++ ){
 				for( var j = 1; j < inflexionPoints2.length; j++ ){
 					var p1 = new BezierPoint( inflexionPoints1[i-1], bezier1 );
 					var p2 = new BezierPoint( inflexionPoints1[i], bezier1 );
 					var p3 = new BezierPoint( inflexionPoints2[j-1], bezier2 );
 					var p4 = new BezierPoint( inflexionPoints2[j], bezier2 );
-					var resultat = find2( bezier1, bezier2, p1, p2, p3, p4 );
-					solutions = ajouterSolutions( solutions, resultat )
+					var result = findIntersectionByDichotomyBezier( bezier1, bezier2, p1, p2, p3, p4 );
+					solutions = ajouterSolutions( solutions, result )
 				}
 			}
 		}
-
-
 		if( solutions.length != 0 ){
 				var intersectionData = new IntersectionData( "point", solutions );
 			} else {
---
+++ testCourbeCourbe.js
@@ -0,0 +1,85 @@
+var test = [] 
+test[0] = 
+	{ name : "Test Intersection Path horizontal 1 : no intersection top"
+	, target : "intersectionBezierBezier"
+	, input : function( svg ) { 
+			var path1 = svg.path( 'M 25 50 C 40 15 70 25 90 20' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			var path2 = svg.path( 'M 15 40 C 30 40 80 30 80 10' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			return [ path1, path2 ];
+		}
+	, output : [ [ ] ]
+	}
+test[1] = 
+	{ name : "Test Intersection Path horizontal 1 : no intersection top"
+	, target : "intersectionBezierBezier"
+	, input : function( svg ) { 
+			var path1 = svg.path( 'M 25 50 C 50 15 75 80 100 55' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			var path2 = svg.path( 'M 30 50 C 45 5 70 100 95 40' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			return [ path1, path2 ];
+		}
+	, output : [ [ ] ]
+	}
+test[2] = 
+	{ name : "Test Intersection Path horizontal 1 : no intersection top"
+	, target : "intersectionBezierBezier"
+	, input : function( svg ) { 
+			var path1 = svg.path( 'M 25 50 C 50 15 75 80 100 45' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			var path2 = svg.path( 'M 20 30 C 45 25 110 60 110 65' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			return [ path1, path2 ];
+		}
+	, output : [ [ ] ]
+	}
+test[3] = 
+	{ name : "Test Intersection Path horizontal 1 : no intersection top"
+	, target : "intersectionBezierBezier"
+	, input : function( svg ) { 
+			var path1 = svg.path( 'M 25 50 C 50 5 75 80 100 45' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			var path2 = svg.path( 'M 20 30 C 45 35 110 60 110 65' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			return [ path1, path2 ];
+		}
+	, output : [ [ ] ]
+	}
+test[4] = 
+	{ name : "Test Intersection Path horizontal 1 : no intersection top"
+	, target : "intersectionBezierBezier"
+	, input : function( svg ) { 
+			var path1 = svg.path( 'M 25 50 C 50 5 75 80 100 45' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			var path2 = svg.path( 'M 20 40 C 45 35 110 60 110 65' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			return [ path1, path2 ];
+		}
+	, output : [ [ ] ]
+	}
+test[5] = 
+	{ name : "Test Intersection Path horizontal 1 : no intersection top"
+	, target : "intersectionBezierBezier"
+	, input : function( svg ) { 
+			var path1 = svg.path( 'M 25 50 C 50 5 75 80 100 45' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			var path2 = svg.path( 'M 25 35 C 200 40 -150 45 110 50' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			return [ path1, path2 ];
+		}
+	, output : [ [ ] ]
+	}
+test[6] = 
+	{ name : "Test Intersection Path horizontal 1 : no intersection top"
+	, target : "intersectionBezierBezier"
+	, input : function( svg ) { 
+			var path1 = svg.path( 'M 25 60 C 50 -15 75 100 120 35' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			var path2 = svg.path( 'M 25 40 C 400 40 -200 45 110 50' )
+				 .stroke( { width : 1 } ).fill( "none" );
+			return [ path1, path2 ];
+		}
+	, output : [ [ ] ]
+	}
